abb9ae9f039540ac6befbcf9dc96fd68344b8ede
Pablo Meyer <pablomeyer@gmail.com>
2017-11-04 19:07:50 -0300

    directive properties and binding

--- src/app/advanced-highlight/advanced-highlight.directive.ts
+++ src/app/advanced-highlight/advanced-highlight.directive.ts
@@ -1,24 +1,26 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAdvancedHighlight]'
 })
 export class AdvancedHighlightDirective implements OnInit {
-
-  @HostBinding('style.backgroundColor') backgroundColor = 'pink';
+  @Input() color1: string;
+  @Input('appAdvancedHighlight') color2: string;
+  @HostBinding('style.backgroundColor') backgroundColor;
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
   ngOnInit(): void {
+    this.backgroundColor = this.color1;
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
-    this.backgroundColor = 'pink';
+    this.backgroundColor = this.color2;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     //  this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.color1;
   }
 }
