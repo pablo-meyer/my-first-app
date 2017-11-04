212eda9dc589b450ff67676201c2dbbdabf85cf2
Pablo Meyer <pablomeyer@gmail.com>
2017-11-04 16:57:05 -0300

    using host binding to bind to host properties

--- src/app/advanced-highlight/advanced-highlight.directive.ts
+++ src/app/advanced-highlight/advanced-highlight.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appAdvancedHighlight]'
 })
 export class AdvancedHighlightDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') backgroundColor = 'pink';
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
   ngOnInit(): void {
@@ -12,10 +13,12 @@ export class AdvancedHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'pink');
+    this.backgroundColor = 'pink';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    //  this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
