33013a43e3686b2bd75622b2c3d2c7de7e244717
Pablo Meyer <pablomeyer@gmail.com>
2017-11-05 18:47:05 -0300

    Displaying ingredients in recipe

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -4,11 +4,16 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        // tslint:disable-next-line:max-line-length
         new Recipe('Cake', 'Chocolate Cake', 'https://cdn.pinchofyum.com/wp-content/uploads/Chocolate-Cake-5-1-Yoast.jpg',
-        [new Ingredient('Water', 200), new Ingredient('Flour', 100)]),
+        [
+            new Ingredient('Water', 200),
+            new Ingredient('Flour', 100)
+        ]),
         new Recipe('Fake Cake', 'Chocolate Cake', 'https://cdn.pinchofyum.com/wp-content/uploads/Chocolate-Cake-5-1-Yoast.jpg',
-        [new Ingredient('Chocolate', 200), new Ingredient('Sugar', 100)]),
+        [
+            new Ingredient('Chocolate', 200),
+            new Ingredient('Sugar', 100)
+        ]),
     ];
     recipeClicked = new EventEmitter<Recipe>();
     getRecipes() {
