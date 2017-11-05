39c55baed2dd708afb673142bfdb1a25e29de5b3
Pablo Meyer <pablomeyer@gmail.com>
2017-11-05 18:30:23 -0300

    Added ingredients t recipes

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Recipe } from '../recipes/recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('Cake', 'Chocolate Cake', 'https://cdn.pinchofyum.com/wp-content/uploads/Chocolate-Cake-5-1-Yoast.jpg'),
-        new Recipe('Fake Cake', 'Chocolate Cake', 'https://cdn.pinchofyum.com/wp-content/uploads/Chocolate-Cake-5-1-Yoast.jpg')
+        // tslint:disable-next-line:max-line-length
+        new Recipe('Cake', 'Chocolate Cake', 'https://cdn.pinchofyum.com/wp-content/uploads/Chocolate-Cake-5-1-Yoast.jpg',
+        [new Ingredient('Water', 200), new Ingredient('Flour', 100)]),
+        new Recipe('Fake Cake', 'Chocolate Cake', 'https://cdn.pinchofyum.com/wp-content/uploads/Chocolate-Cake-5-1-Yoast.jpg',
+        [new Ingredient('Chocolate', 200), new Ingredient('Sugar', 100)]),
     ];
     recipeClicked = new EventEmitter<Recipe>();
     getRecipes() {
